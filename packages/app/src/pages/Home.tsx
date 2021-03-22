1935f483b27ae15498774b26f8a745e8a4eb0a98
Bryan Garzon <bryan.garzon@ticketmaster.com>
2021-03-22 13:35:21 -0500

    feat(app): i18n translation setup
    base setup for i18n, update dependencies

---
+++ packages/app/src/pages/Home.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { RouteComponentProps } from "@reach/router";
+import { useTranslation } from "react-i18next";
+
+type IHome = RouteComponentProps;
+
+const Home: React.FC<IHome> = () => {
+  const { t } = useTranslation();
+
+  return (
+    <section>
+      <h1>{t("welcome", "hello there")}</h1>
+      <p>{t("description", "short description")}</p>
+    </section>
+  );
+};
+
+export default Home;
